4a4220960438c1b54a3eb3745b550a27f926a992
sureshsahu <suresh.sahu@trangile.com>
2023-02-01 02:03:24 +0530

    addeed

--- lib/aws_cdk_test-stack.ts
+++ lib/aws_cdk_test-stack.ts
@@ -1,19 +1,47 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { SecretValue } from 'aws-cdk-lib';
+// import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
+// import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+// import { Action, Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+// import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+// import { Construct } from 'constructs';
+// import { Topic } from "aws-cdk-lib/aws-sns";
+// import { EmailSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
+// // import * as sqs from 'aws-cdk-lib/aws-sqs';
+// // import { SnsTopic } from 'aws-cdk-lib/aws-events-targets';
+// // import { EventField, RuleTargetInput } from 'aws-cdk-lib/aws-events';
 import * as cdk from 'aws-cdk-lib';
 import { SecretValue } from 'aws-cdk-lib';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
-import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { Action, Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
 import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { EmailSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 
 export class AwsCdkTestStack extends cdk.Stack {
+  private readonly pipelineNotificationsTopic: Topic;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
     const pipeline=new Pipeline(this,"Pipeline",{
       pipelineName: 'newmanpipeline',
       crossAccountKeys: false,
       restartExecutionOnUpdate: true,
     })
+    this.pipelineNotificationsTopic = new Topic(
+      this,
+      "PipelineNotificationsTopic",
+      {
+        topicName: "PipelineNotifications",
+      }
+    );
+
+    this.pipelineNotificationsTopic.addSubscription(
+      new EmailSubscription("suresh.sahu@trangile.com")
+    );
+
     const cdkSourceOutput=new Artifact("CDKSourceOutput")
     pipeline.addStage({
       stageName:"source",
@@ -30,7 +58,7 @@ export class AwsCdkTestStack extends cdk.Stack {
       ]
     })
     const cdkBuildOutput=new Artifact("CdkBuildOutPut")
-    pipeline.addStage({
+    const buildStage=pipeline.addStage({
       stageName:"build",
       actions: [
         new CodeBuildAction({
@@ -46,9 +74,12 @@ export class AwsCdkTestStack extends cdk.Stack {
               "build-specs/cdk-newman-build-spec.yml"
             ),
           }),
+          
+         
         }),
       ]
     })
+    
     pipeline.addStage({
       stageName: "Pipeline_Update",
       actions: [
@@ -59,8 +90,7 @@ export class AwsCdkTestStack extends cdk.Stack {
           adminPermissions: true,
         }),
       ],
+      
     });
-
-
   }
 }
