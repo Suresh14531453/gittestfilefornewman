ffd36de7bda446d8f09151ee074ed3d408042035
sureshsahu <suresh.sahu@trangile.com>
2023-02-05 20:53:55 +0530

    test_mail

--- lib/aws_cdk_test-stack.ts
+++ lib/aws_cdk_test-stack.ts
@@ -1,95 +1,3 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { SecretValue } from 'aws-cdk-lib';
-// import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
-// import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
-// import { Action, Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
-// import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
-// import { Construct } from 'constructs';
-// import { Topic } from "aws-cdk-lib/aws-sns";
-// import { EmailSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
-// // import * as sqs from 'aws-cdk-lib/aws-sqs';
-// // import { SnsTopic } from 'aws-cdk-lib/aws-events-targets';
-// // import { EventField, RuleTargetInput } from 'aws-cdk-lib/aws-events';
-// import * as cdk from 'aws-cdk-lib';
-// import { SecretValue } from 'aws-cdk-lib';
-// import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
-// import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
-// import { Action, Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
-// import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
-// import { Construct } from 'constructs';
-
-
-// export class AwsCdkTestStack extends cdk.Stack {
-  
-//   // private readonly pipelineNotificationsTopic: Topic;
-//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-//     super(scope, id, props);
-//     const pipeline=new Pipeline(this,"Pipeline",{
-//       pipelineName: 'newmanpipeline',
-//       crossAccountKeys: false,
-//       restartExecutionOnUpdate: true,
-//     })
-    
-
-//     const cdkSourceOutput=new Artifact("CDKSourceOutput")
-//     pipeline.addStage({
-//       stageName:"source",
-//       actions:[
-//         new GitHubSourceAction({
-//           owner: "Suresh14531453",
-//             repo: "gittestfilefornewman",
-//             branch: "master",
-//             actionName: "Pipeline_Source",
-//             oauthToken: SecretValue.secretsManager("token_access"),
-//             output: cdkSourceOutput
-//         }
-//         )
-//       ]
-//     })
-//     const email = 'your.email@example.com';
-//     const emailSubject = 'Build Stage Failed';
-//     const emailText = 'Your pipeline has failed.';
-//     const emailHtml = '<p>Your pipeline has failed.</p>';
-
-//     const cdkBuildOutput=new Artifact("CdkBuildOutPut")
-//     pipeline.addStage({
-//       stageName:"build",
-//       actions: [
-//         new CodeBuildAction({
-
-//           actionName: "CDK_Build",
-//           input: cdkSourceOutput,
-//           outputs: [cdkBuildOutput],
-//           project: new PipelineProject(this, "CdkBuildProject", {
-//             environment: {
-//               buildImage: LinuxBuildImage.STANDARD_5_0,
-//             },
-//             buildSpec: BuildSpec.fromSourceFilename(
-//               "build-specs/cdk-newman-build-spec.yml"
-//             ),
-//           }),
-          
-         
-//         }),
-//       ]
-//     })
-    
-//     pipeline.addStage({
-//       stageName: "Pipeline_Update",
-//       actions: [
-//         new CloudFormationCreateUpdateStackAction({
-//           actionName: "Pipeline_Update",
-//           stackName: "AwsCdkTestStack",
-//           templatePath: cdkBuildOutput.atPath("AwsCdkTestStack.template.json"),
-//           adminPermissions: true,
-//         }),
-//       ],
-      
-//     });
-   
-//   }
-// }
-
 import * as cdk from 'aws-cdk-lib';
 import { SecretValue, Stage } from 'aws-cdk-lib';
 import { BuildEnvironmentVariableType, BuildSpec, LinuxBuildImage, PipelineProject, Project } from 'aws-cdk-lib/aws-codebuild';
@@ -142,37 +50,6 @@ export class AwsCdkTestStack extends cdk.Stack {
     })
 
     const cdkBuildOutput = new Artifact("CdkBuildOutPut")
-    // pipeline.addStage({
-    //   stageName:"build",
-    //   actions: [
-    //     new CodeBuildAction({
-
-    //       actionName: "CDK_Build",
-    //       input: cdkSourceOutput,
-    //       outputs: [cdkBuildOutput],
-    //       project: new PipelineProject(this, "CdkBuildProject", {
-    //         environment: {
-    //           buildImage: LinuxBuildImage.STANDARD_5_0,
-    //         },
-    //         buildSpec: BuildSpec.fromSourceFilename(
-    //           "build-specs/cdk-newman-build-spec.yml"
-    //         ),
-    //       }),
-    //     }),
-    //   ],
-    //   // onFail: new SnsAction({
-    //   //       actionName: 'NotifyFailure',
-    //   //       topic: snsTopic,
-    //   //       message: pipeline.ArtifactPath.fromSourcePath('Build failed')
-    //   //     })
-    //   new SnsAction({
-    //     actionName: "NotifyFailure",
-    //     topic: snsTopic,
-    //     message: "Build failed",
-    //     runOrder: 2,
-    //   }),
-      
-    // })
     const buildStage=pipeline.addStage({
       stageName: "build",
       actions: [
@@ -192,44 +69,6 @@ export class AwsCdkTestStack extends cdk.Stack {
        
       ],
     });
-    // buildStage.onStateChange(
-    //   "FAILED",
-    //   new SnsTopic(this.pipelineNotificationsTopic, {
-    //     message: RuleTargetInput.fromText(
-    //       `Build Test Failed By Syed. See details here: ${EventField.fromPath(
-    //         "$.detail.execution-result.external-execution-url"
-    //       )}`
-    //     ),
-    //   }),
-    //   {
-    //     ruleName: "Failed",
-    //     eventPattern: {
-    //       detail: {
-    //         state: ["FAILED"],
-    //       },
-    //     },
-    //     description: "Integration test has failed by syed",
-    //   }
-    // );
-    // buildStage.onStateChange(
-    //   "Succeeded",
-    //   new SnsTopic(this.pipelineNotificationsTopic, {
-    //     message: RuleTargetInput.fromText(
-    //       `Build Test success. See details here: ${EventField.fromPath(
-    //         "$.detail.execution-result.external-execution-url"
-    //       )}`
-    //     ),
-    //   }),
-    //   {
-    //     ruleName: "SUCCEDED",
-    //     eventPattern: {
-    //       detail: {
-    //         state: ["SUCCEEDED"],
-    //       },
-    //     },
-    //     description: "Integration test has SUCCESS by SURESH",
-    //   }
-    // );
     const snsTopic = new SnsTopic(this.pipelineNotificationsTopic, {
       message: RuleTargetInput.fromText(
         `Build Test Failed`
@@ -261,7 +100,18 @@ export class AwsCdkTestStack extends cdk.Stack {
       },
       description: "Build Test Successful",
     });
-    
+    pipeline.addStage({
+      stageName: "Pipeline_Update",
+      actions: [
+        new CloudFormationCreateUpdateStackAction({
+          actionName: "Pipeline_Update",
+          stackName: "AwsCdkTestStack",
+          templatePath: cdkBuildOutput.atPath("AwsCdkTestStack.template.json"),
+          adminPermissions: true,
+        }),
+      ],
+
+    });
     
 
   }
